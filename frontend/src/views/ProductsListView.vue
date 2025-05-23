eb77a10ff426f979cbd1b0926e2f2591121453e8
LEELEE412 <78653980+LEELEE412@users.noreply.github.com>
2025-05-23 17:59:01 +0900

    Update products view with saving products support

--- frontend/src/views/ProductsListView.vue
+++ frontend/src/views/ProductsListView.vue
@@ -52,7 +52,7 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted, watch } from 'vue';
 import axios from 'axios';
 import ProductDetailModal from '@/components/ProductDetailModal.vue';
 
